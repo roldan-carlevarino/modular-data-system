8c440d899e500282d4b6e40326ca7ba3464adff2
Rodrigo Roldán <rrollpk@gmail.com>
2026-02-07 14:04:41 +0100

    Add files via upload

--- api.py
+++ api.py
@@ -839,3 +839,119 @@ def pando_event():
         
         raise HTTPException(status_code=500, detail=str(e))
 
+
+@app.get("/shopping/items")
+def get_all_items():
+    conn = None
+    cur = None
+    
+    try:
+        conn = psycopg2.connect(os.getenv("TASKS_URL"), sslmode="require")
+        cur = conn.cursor()
+
+        cur.execute("""
+            SELECT item FROM shopping_food
+                    WHERE active = true
+            UNION ALL
+            SELECT item FROM shopping_others
+                    WHERE active = true
+        """)
+        rows = cur.fetchall()
+
+        return [r[0] for r in rows]
+        
+    except Exception as e:
+        raise HTTPException(500, f"Failed to get items: {str(e)}")
+        
+    finally:
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
+
+@app.get("/shopping/list")
+def get_shopping_list():
+    conn = None
+    cur = None
+    
+    try:
+        conn = psycopg2.connect(os.getenv("TASKS_URL"), sslmode="require")
+        cur = conn.cursor()
+
+        cur.execute("""
+            SELECT item 
+            FROM shopping_list
+            """)
+        rows = cur.fetchall()
+
+        return [r[0] for r in rows]
+        
+    except Exception as e:
+        raise HTTPException(500, f"Failed to get shopping list: {str(e)}")
+        
+    finally:
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
+
+@app.post("/shopping/insert_list")
+def insert_shopping_list(items: list[str]):
+    conn = None
+    cur = None
+    
+    try:
+        conn = psycopg2.connect(os.getenv("TASKS_URL"), sslmode="require")
+        cur = conn.cursor()
+
+        for item in items:
+            cur.execute("""
+                INSERT INTO shopping_list
+                (item)
+                VALUES (%s)
+                ON CONFLICT (item)
+                DO NOTHING;
+            """, (item,))
+
+        conn.commit()
+        return {"ok": True}
+        
+    except Exception as e:
+        if conn:
+            conn.rollback()
+        raise HTTPException(500, f"Failed to insert items: {str(e)}")
+        
+    finally:
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
+
+@app.post("/shopping/delete_list")
+def delete_shopping_list(items: list[str]):
+    conn = None
+    cur = None
+    
+    try:
+        conn = psycopg2.connect(os.getenv("TASKS_URL"), sslmode="require")
+        cur = conn.cursor()
+
+        for item in items:
+            cur.execute("""
+                DELETE FROM shopping_list
+                WHERE item = %s
+            """, (item,))
+
+        conn.commit()
+        return {"ok": True}
+        
+    except Exception as e:
+        if conn:
+            conn.rollback()
+        raise HTTPException(500, f"Failed to delete items: {str(e)}")
+        
+    finally:
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()

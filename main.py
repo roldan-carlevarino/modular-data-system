0921b49b8879d15dd1e94fe49670fe5e957324fc
Rodrigo Roldán <rrollpk@gmail.com>
2026-02-20 15:48:25 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -22,7 +22,9 @@ app = FastAPI()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://rrollpk.github.io"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
